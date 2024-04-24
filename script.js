41fd7323f9cd338f5496cba57de5133446deae58
Sai Ganesh Pendela <spendela@umd.edu>
2024-04-24 13:24:29 -0400

    Implement the Play Game Function

--- script.js
+++ script.js
@@ -7,63 +7,77 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-function getHumanChoice(){
-    let humanChoice = prompt("Enter you choice, Rock, Paper or Scissor");
-    humanChoice = humanChoice.toLowerCase();
-    return humanChoice;
+
+function playGame(){
+
+    function getHumanChoice(){
+        let humanChoice = prompt("Enter you choice, Rock, Paper or Scissor");
+        humanChoice = humanChoice.toLowerCase();
+        return humanChoice;
+    }
+    
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice=="rock" && computerChoice=="paper"){
+            humanScore+=1;
+            return("You lose! Paper beats Rock");
+        }
+        else if(humanChoice=="paper" && computerChoice=="rock"){
+            computerScore+=1;
+            return("You Win! Paper beats Rock");
+        }
+    
+        else if(humanChoice=="rock" && computerChoice=="scissor"){
+            humanChoice+=1;
+            return("You win! rock beats scissor");
+        }
+    
+        else if(humanChoice=="scissor" && computerChoice=="rock"){
+            computerChoice+=1;
+            return("You loose! rock beats scissor");
+        }
+    
+        else if(humanChoice=="paper" && computerChoice=="scissor"){
+            computerChoice+=1;
+            return("You loose! scissor beats paper");
+        }
+    
+        else if(humanChoice=="scissor" && computerChoice=="paper"){
+            humanChoice+=1;
+            return("You win! scissor beats paper");
+        }
+    
+        else if(humanChoice=="scissor" && computerChoice=="scissor"){
+            return("Its, a Tie");
+        }
+    
+        else if(humanChoice=="paper" && computerChoice=="paper"){
+            return("Its, a Tie");
+        }
+    
+        else if(humanChoice=="rock" && computerChoice=="rock"){
+            return("Its, a Tie");
+        }
+    
+    }
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for(let i=1; i<=5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        alert(playRound(humanSelection, computerSelection));
+    }
+
+    if (humanScore>computerScore){
+        alert("You Won !");
+    }
+    else if(computerScore>humanScore){
+        alert("You loose");
+    }
+    else{
+        alert("It's a tie.")
+    }
 }
 
-let humanScore = 0;
-let computerScore =0;
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice=="rock" && computerChoice=="paper"){
-        humanScore+=1;
-        return("You lose! Paper beats Rock");
-    }
-    else if(humanChoice=="paper" && computerChoice=="rock"){
-        computerScore+=1;
-        return("You Win! Paper beats Rock");
-    }
-
-    else if(humanChoice=="rock" && computerChoice=="scissor"){
-        humanChoice+=1;
-        return("You win! rock beats scissor");
-    }
-
-    else if(humanChoice=="scissor" && computerChoice=="rock"){
-        computerChoice+=1;
-        return("You loose! rock beats scissor");
-    }
-
-    else if(humanChoice=="paper" && computerChoice=="scissor"){
-        computerChoice+=1;
-        return("You loose! scissor beats paper");
-    }
-
-    else if(humanChoice=="scissor" && computerChoice=="paper"){
-        humanChoice+=1;
-        return("You win! scissor beats paper");
-    }
-
-    else if(humanChoice=="scissor" && computerChoice=="scissor"){
-        return("Its, a Tie");
-    }
-
-    else if(humanChoice=="paper" && computerChoice=="paper"){
-        return("Its, a Tie");
-    }
-
-    else if(humanChoice=="rock" && computerChoice=="rock"){
-        return("Its, a Tie");
-    }
-
-
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log(humanSelection);
-console.log(computerSelection);
-alert(playRound(humanSelection, computerSelection));
+playGame();
