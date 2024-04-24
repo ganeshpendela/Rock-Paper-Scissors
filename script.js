c93550f825b0417635959c35742cc18cebba827c
Sai Ganesh Pendela <spendela@umd.edu>
2024-04-24 12:51:04 -0400

    Implement getHumanChoice, getComputerChoice fun.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hi");
+
+function getComputerChoice(){
+    let options = ["rock","paper","scissor"];
+    let randomNumber = Math.floor(Math.random()*3);
+    let computerChoice = options[randomNumber];
+    console.log(computerChoice);
+}
+
+getComputerChoice();
+
+function getHumanChoice(){
+    let humanChoice = prompt("Enter you choice, Rock, Paper or Scissor");
+    console.log(humanChoice);
+}
+
+getHumanChoice();
